03d2ac3184d0b5128fe7e967f17853d5e3f46c84
void2610 <izumisan2610@gmail.com>
2023-09-13 00:40:47 +0900

    頭データ完成

---
+++ acvi-randomizer/src/data/PartsData.ts
@@ -0,0 +1,26 @@
+import { PartsType } from "../types/Parts";
+
+export const HeadData: PartsType[] = [
+  { name: "HD-011 MELANDER", category: "", image: "https://lh3.googleusercontent.com/qBtFFpHHsEq9jNRIK3kZyViovMJg-qtN69cmmgKvXRtxtT_8bhLxeytv5lyAU1rZ6T8w8dJ12-nP_kh3vgDb8DAIP5VxPnheVQA1eexcHOoN=rw" },
+  { name: "HC-2000 FINDER EYE", category: "", image: "https://lh3.googleusercontent.com/PSFalqRASa_Ob_r_l29RGzRFIY9DWW0eDfxAZh5Obwt_LxThePiLvAv3apCnHOi1r8a4wR657xrVHHtVh8nSeQMF2rN9CLCZRx-vLIfvR2TVcw=rw" },
+  { name: "AH-J-124 BASHO", category: "", image: "https://lh3.googleusercontent.com/CqH5yKbLKvguD3uZ5QAxgCHqlfuJR5BzgeaVeAbx3qnE-kZ7eKR3mjTOOxv7rXaNKWJXhDZu-7Hsg9B0ofajEvFR1gbSq8706OnGWGRWpds=rw" },
+  { name: "DF-HD-08 TIAN-QIANG", category: "", image: "https://lh3.googleusercontent.com/8Y-Z5G9CxEiwkTzCnEELKODTZVnqZoFwMeuTJEQatz44qQFav8jQbWqjbuc8eue4buJZ52qNlXgjhXPdAB7IolcQD3_adyiTL2dhr6NHQ3I=rw" },
+  { name: "VP-44S", category: "", image: "https://lh3.googleusercontent.com/u9J-5CjfGPJcWihGLLxBHtfB0WtDMpGpojco6EoYNx26xIcghG1xI5JnUQy150SNznzBGcqIoYGspmbYSJF-GeVyj2jucfcRwSlo4AoHEsbrcg=rw" },
+  { name: "NACHTREIHER/44E", category: "", image: "https://lh3.googleusercontent.com/QNrH0gHq1zFOhK7Bvq3rmEXbJIXQBbPOtZ7Gsk-KpDU4gHolR2U1oCtAcjf0W7AfppnQ-L4VGlmk46rrSkWGB_qOXGLpvNXfTfjUT_1jyggF=rw" },
+  { name: "KASUAR/44Z", category: "", image: "https://lh3.googleusercontent.com/jnpAECqqFe2dyfwhVPpu-UiuU2rr9hh99UI0do9E2TDKvH0j09GAQ30uT4zbaoqw2zY06kDihJosI9fnJyO2xuFlygA9JDsQCd9TlP5ITjbK=rw" },
+  { name: "HC-3000 WRECKER", category: "", image: "https://lh3.googleusercontent.com/aiqH9SJLjlr3enilj3McMssIrCtc25TqTl2xWTTwrRhRnSmeQQ7ySKZStODAMFWmIHU2gjB4H05xhuvfxhc7OxZG3cFjiIthgq3HAev3VLgq=rw" },
+  { name: "HD-012 MELANDER C3", category: "", image: "https://lh3.googleusercontent.com/3joGI1nAomk_dPR5FatmCj2_1Ar1B9ut8K7ZEM-em5-vpPeVG_3w1BfM-Fw1Xw082z9MZtBgDEVNXH0m77trRZDAG0xnAjbL1u0J9idPKikg=rw" },
+  { name: "IA-C01H:EPHEMERA", category: "", image: "https://lh3.googleusercontent.com/yPthX0MCg-uIzHNX7H6QLup9_Tx293m_mBEVtVH259W6XQyz8Hya-Qew5pAVZpdWt1kLYgN0wKuCZjC8zWF8ua7ojaS-2xbYVPsHON8I1jx1=rw" },
+  { name: "IB-C03H:HAL 826", category: "", image: "https://lh3.googleusercontent.com/eQUZAqxQLvqyzYBQrbTisyorXK89WLp9M3YqVEXxG6AwwJQBEBWfaeLYIWE3P-sxj2lYofm2FpUc2iG-QEOagzRDZANODyNZGe_n9-epe_U=rw" },
+  { name: "HS-5000 APPETIZER", category: "", image: "https://lh3.googleusercontent.com/IFEDGOrFV40080Wsb1exED1S7B_5XSwb1ht5_3LvMB2lH3j8sM-0CjPg60rzpqZEseTaesH5JEPmnRedefFZvflgtCd7_VPSUUVfCimkRW8s=rw" },
+  { name: "AH-J-124/RC JAILBREAK", category: "", image: "https://lh3.googleusercontent.com/8IUlgh0Q1Agekg21jzERk6VBs9WCfnDqS48u15ULgLA95EHcZ5OV7GwKzC5rBCK-IjjEsmvqM4kEhDdERf8ixBGKC64UpcFkd8TuCgaRmxA=rw" },
+  { name: "VE-44B", category: "", image: "https://lh3.googleusercontent.com/Uo0RDmQuTthPOVW88PXnSqf3hgGpihQlu4uMY2B2NmGVGwPZQ9WKo8cGtCGtUPOUyx2X-NLEAgSOCdqWXwT4r4IVyYJ63L-Q9sbHjJtAdMP9=rw" },
+  { name: "VP-44D", category: "", image: "https://lh3.googleusercontent.com/7DnZDNao0uVi0oLLDdCsTs9Nw6kicyGHsIK4NAPUIR_KqN1p8Hw_98qD4EKA8BjDyFva9I9mfzr2bD_10doTlMJRmZElQqhPH18ziqkYmOM8=rw" },
+  { name: "VE-44A", category: "", image: "https://lh3.googleusercontent.com/fteDmW7eKZyzq0f0MMEAnMQ3n-aT9wdSa-etVSh6W989oBsrMlESmVhPREYnXdEd5uME8w1E8au1d-eimmy6q_2Mn6wYj6jmLf8Xvmzb2pYYGg=rw" },
+  { name: "HD-033M VERRILL", category: "", image: "https://lh3.googleusercontent.com/iuXIVuqwHi2DCJWcpVlKQr3s_r1aG_1DDc7VxTchUbVYjrQsJ-G4m3D2SxrTGriHF7gPxRZ691vOQWWFcH5wLAHhdeQY_RvCrLdLRZVsHXYC=rw" },
+  { name: "EL-TH-10 FIRMEZA", category: "", image: "https://lh3.googleusercontent.com/8kgF5RGLdjGW12UsYH78p0DLbfAWd47gQ7h2whn7uPD7dV5C_WAU_bcWr_sdhGP598NjIvaV54nvoW45PzzNSTzz0yecjFhILocRyqOVklQ=rw" },
+  { name: "HC-2000/BC SHADE EYE", category: "", image: "https://lh3.googleusercontent.com/8EKGA8UaWaDiiYeZEbZNbjMkFEoUaLeVZwHdSed6l6EP3bbr_tgpb7hOMjC89FZfWbVapCbW5DvKVdeKiZtEW5ZLNuVtLS3UasLxI4eM4bA=rw" },
+  { name: "EL-PH-00 ALBA", category: "", image: "https://lh3.googleusercontent.com/r50YtWJYkIa5FhT1Kyqc3B4Cqaf5fQG6lArWQm_-eA6wu8jPbYLOjRS-fzCvCzVqvzKwoDqP8069ToFXKd9wXQF_xEFicZ5FwVRhe4QbGJM=rw" },
+  { name: "20-081 MIND ALPHA", category: "", image: "https://lh3.googleusercontent.com/ak4oa3xu0Pil_BSqLqlcOzfHVs9wB1IP4GtX6uT85hzboi158k2qWojf2mBDAkRUJFiOhO0Bl7brZyXccX9bTiywPG5W8H2vvR0wHP3uWxMjZQ=rw" },
+  {name: "20-082 MIND BETA", category: "", image: "https://lh3.googleusercontent.com/Jsj318sXfy_T4h4cBPp5lSaiBdjJMQi2NvnsbuvKQKo1NFLUkUEeJ3DCwE3zovO2WOAPQgT3Bq0jlR1VzcGSpZtTC3LVHeNyh1O-AJwxWU0=rw" },
+];
